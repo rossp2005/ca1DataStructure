28ab2a35f9a8148637c20456b7777fa3ee7ca63d
rossp2005 <148553377+rossp2005@users.noreply.github.com>
2025-03-17 22:25:06 +0000

    main class where i set the gui visible

---
+++ BloodTestProject.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bloodtestproject;
+
+/**
+ *
+ * @author Ross
+ */
+public class BloodTestProject {
+     public static void main(String[] args) {
+        testScheduler scheduler = new testScheduler();
+        BloodTestGUI myGui = new BloodTestGUI();
+        myGui.setVisible(true);
+    }
+}
+
